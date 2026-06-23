4cd41b091707273b2035efbb4ef98219cf8933a9
Gilberto Ferreira <vivace9842@gmail.com>
2026-06-22 22:31:27 -0300

    add health check

--- api/health.js
+++ api/health.js
@@ -1,7 +1,7 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    status: "online",
-    project: "MFRGS Services",
-    version: "1.0.0"
+export default function handler(req, res) {
+  res.json({ 
+    status: "online", 
+    project: "MFRGS Services", 
+    version: "1.0.0" 
   });
-};
+}
