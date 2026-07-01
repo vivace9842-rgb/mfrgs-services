7300c78810fb7fa7f3fb910ea4c10314bf305035
vivace9842-rgb <vivace9842@gmail.com>
2026-07-01 19:06:59 -0300

    Refactor webhook handler to use ES6 modules

--- src/routes/webhookr.js
+++ src/routes/webhookr.js
@@ -1,6 +1,8 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+import Stripe from "stripe";
 
-module.exports = async function handler(req, res) {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -38,4 +40,4 @@ module.exports = async function handler(req, res) {
   }
 
   return res.status(200).json({ received: true });
-};
+}
